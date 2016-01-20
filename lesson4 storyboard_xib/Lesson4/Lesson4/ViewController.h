affbe3e9d89a9d37d667281607cd4063946f52c9
Yalin <523031393@qq.com>
2016-01-20 21:32:32 +0800

    some

---
+++ lesson4 storyboard_xib/Lesson4/Lesson4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson4
+//
+//  Created by Yalin on 16/1/20.
+//  Copyright © 2016年 Yalin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ lesson4 storyboard_xib/Lesson4/Lesson4/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lesson4
+//
+//  Created by Yalin on 16/1/20.
+//  Copyright © 2016年 Yalin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
